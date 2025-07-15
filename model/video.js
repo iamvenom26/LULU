07e56be30d5fb9bc26ceaddf70a3544d8d60686d
Mohd. Ziya <mzziya2004@gmail.com>
2025-07-16 01:53:05 +0530

    Update video.js

--- model/video.js
+++ model/video.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const videoSchema = new mongoose.Schema({
     title: { type: String, required: true },
     videoUrl: { type: String, required: true },
+    coverProfile: { type: String }, 
     course: { type: mongoose.Schema.Types.ObjectId, ref: "Course" } // Each video belongs to a Course
 });
 
